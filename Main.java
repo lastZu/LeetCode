ff633e8d85e2bd515ead040ad493a2fd0b36410c
lastKraken <cdrom2015@ya.ru>
2022-11-27 13:50:43 +0300

    TwoSum

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
+import java.awt.*;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int sum = MaximumSubarray.maxSubArray(new int[] {8, -20, 5, -2, 7});
-        System.out.println(sum);
+        int[] sum = TwoSum.twoSumWithSort(new int[] {8, -20, 5, -2, 7}, -22);
+        System.out.println(Arrays.toString(sum));
     }
 }
