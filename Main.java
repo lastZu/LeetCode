fb1a1d1510a0e3fd5fe59faa165ebf58f22d5491
lastKraken <cdrom2015@ya.ru>
2022-11-30 00:26:12 +0300

    ReshapeTheMatrix

--- Main.java
+++ Main.java
@@ -4,8 +4,10 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] num1 = new int[] {4, 9, 5, 1, 2, 3, 1, 5, 10};
-        int[] num2 = new int[] {9, 4, 9, 8, 4};
-        int num = BestTimeToBuyAndSellStock.maxProfit (num1);
-        System.out.println(num);
+        int[][] num2 = new int[][] {{9, 4, 9}, {8, 4, 4}};
+        int[][] num = ReshapeTheMatrix.matrixReshape(num2, 1, 6);
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(Arrays.toString(num[i]));
+        }
     }
 }
