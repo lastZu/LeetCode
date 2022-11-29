6ff6482400d51aaebd19a0f95233803f00be4e7c
lastKraken <cdrom2015@ya.ru>
2022-11-29 12:30:53 +0300

    BestTimeToBuyAndSellStock

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] num1 = new int[] {4, 9, 5};
+        int[] num1 = new int[] {4, 9, 5, 1, 2, 3, 1, 5, 10};
         int[] num2 = new int[] {9, 4, 9, 8, 4};
-        int[] num = IntersectionOfTwoArrays2.intersect (num1, num2);
-        System.out.println(Arrays.toString(num));
+        int num = BestTimeToBuyAndSellStock.maxProfit (num1);
+        System.out.println(num);
     }
 }
