a439828dfba8d0e66396c8d65585f65ff2c6c4e1
lastKraken <cdrom2015@ya.ru>
2022-11-30 16:18:44 +0300

    PascalsTriangle

--- Main.java
+++ Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
         int[] num1 = new int[] {4, 9, 5, 1, 2, 3, 1, 5, 10};
         int[][] num2 = new int[][] {{9, 4, 9}, {8, 4, 4}};
-        int[][] num = ReshapeTheMatrix.matrixReshape(num2, 1, 6);
-        for (int i = 0; i < num.length; i++) {
-            System.out.println(Arrays.toString(num[i]));
+        var num = PascalsTriangle.generate(4);
+        for (int i = 0; i < num.size(); i++) {
+            System.out.println(num.get(i).toString());
         }
     }
 }
