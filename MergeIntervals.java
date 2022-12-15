a3fb9a9af2db3d16eb22a5701b3d0057c1f806c8
lastKraken <cdrom2015@ya.ru>
2022-12-15 22:55:19 +0300

    MergeIntervals2

--- MergeIntervals.java
+++ MergeIntervals.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
@@ -45,6 +42,40 @@ public class MergeIntervals {
             }
             ansver.add(temp);
             temp = new int[]{intervals[i][0], intervals[i][1]};
+        }
+        ansver.add(temp);
+        return ansver.toArray(new int[0][0]);
+    }
+
+    public static int[][] merge2(int[][] intervals) {
+        int max = 0;
+        for (int[] interval : intervals) {
+            max = Math.max(max, interval[1]);
+        }
+        int[] starts = new int[max + 1];
+        int[] ends = new int[max + 1];
+
+        for (int[] interval : intervals) {
+            starts[interval[0]] += 1;
+            ends[interval[1]] += 1;
+        }
+        List<int[]> ansver = new ArrayList<>();
+        int[] temp = new int[2];
+        int openCloseCounter = -1;
+        for (int i = 0; i <= max; i++) {
+            if (starts[i] > 0 && openCloseCounter <= 0) {
+                if (openCloseCounter != -1) {
+                    ansver.add(temp);
+                }
+                temp = new int[2];
+                temp[0] = i;
+                if (openCloseCounter == -1) openCloseCounter = 0;
+            }
+            if (ends[i] > 0) {
+                temp[1] = i;
+            }
+            if (starts[i] > 0) openCloseCounter += starts[i];
+            if (ends[i] > 0) openCloseCounter -= ends[i];
         }
         ansver.add(temp);
         return ansver.toArray(new int[0][0]);
