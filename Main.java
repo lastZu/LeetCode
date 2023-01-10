df2b018f93ed2cbe07a74163274a6e80ee91dd61
lastKraken <cdrom2015@ya.ru>
2023-01-10 21:42:06 +0300

    FindFirstAndLastPositionOfElementInSortedArray

--- Main.java
+++ Main.java
@@ -1,13 +1,11 @@
-import java.awt.*;
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        int[] num1 = new int[] {4, 9, 5, 1, 2, 3, 1, 5, 10};
-        int[][] num2 = new int[][] {{9, 4, 9}, {8, 4, 4}};
-        var num = PascalsTriangle.generate(4);
-        for (int i = 0; i < num.size(); i++) {
-            System.out.println(num.get(i).toString());
-        }
+        // Find First and Last Position of Element in Sorted Array
+        int[] nums = new int[] {1, 3, 3, 4, 6, 9};
+        int[] fl11 = FindFirstAndLastPositionOfElementInSortedArray.searchRange(new int[] {1}, 1);
+        int[] fl3 = FindFirstAndLastPositionOfElementInSortedArray.searchRange(nums, 3);
+        int[] fl1 = FindFirstAndLastPositionOfElementInSortedArray.searchRange(nums, 1);
+        int[] fl5 = FindFirstAndLastPositionOfElementInSortedArray.searchRange(nums, 5);
+        int[] flVoid = FindFirstAndLastPositionOfElementInSortedArray.searchRange(new int[] {}, 3);
     }
 }
